864fad8caa27258715ca08ac6b4113cf58bb9bf2
ikegq <1278822613@qq.com>
2023-10-19 10:34:09 +0800

    1019

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -16,26 +16,38 @@ export default defineUserConfig({
     lastUpdatedText: "",
     // series 为原 sidebar
     series: {
-      "/docs/theme-reco/": [
+      // "/docs/theme-reco/": [
+      //   {
+      //     text: "module one",
+      //     children: ["home", "theme"],
+      //   },
+      //   {
+      //     text: "module two",
+      //     children: ["api", "plugin"],
+      //   },
+      // ],
+      "/docs": [
         {
-          text: "module one",
-          children: ["home", "theme"],
+          text: 'NestJs',
+          children: ['/docs/nest/001', '/docs/nest/002']
         },
         {
-          text: "module two",
-          children: ["api", "plugin"],
-        },
-      ],
+          text: 'NuxtJs',
+          children: ['/docs/nuxt/001', '/docs/nuxt/002']
+        }
+      ]
     },
     navbar: [
       { text: "Home", link: "/" },
-      { text: "Categories", link: "/categories/reco/1/" },
-      { text: "Tags", link: "/tags/tag1/1/" },
+      { text: "Categories", link: "/categories/html/1/" },
+      { text: "Tags", link: "/tags/html/1/" },
       {
         text: "Docs",
         children: [
-          { text: "vuepress-reco", link: "/docs/theme-reco/theme" },
-          { text: "vuepress-theme-reco", link: "/blogs/other/guide" },
+          // { text: "vuepress-reco", link: "/docs/theme-reco/theme" },
+          { text: "NestJs", link: "/docs/nest/001" },
+          { text: "NuxtJs", link: "/docs/nuxt/001" },
+          // { text: "vuepress-theme-reco", link: "/blogs/other/guide" },
         ],
       },
     ],
